6b57d9c176887176d77fb1690c3c84d4c8f28acf
sabinamajewska <115459150+sabinamajewska@users.noreply.github.com>
2024-06-10 14:53:17 +0200

    Definition for correct coloring, proof of bicolorability of bipartite graphs and our invention

--- projekt.v
+++ projekt.v
@@ -1,6 +1,7 @@
 Require Import Coq.Lists.List.
 Require Import Coq.Arith.Arith.
 Require Import Lia. 
+Require Import Lra.
 
 (*Record graph := {
   V : Set;
@@ -50,10 +51,8 @@ Fixpoint nodup (l : list nat) : list nat :=
 Eval compute in nodup [1 ; 2; 1].
   
 Definition degree (g : graph) (v : nat) : nat := length( filter (fun x => match x with | Ptrue => true | _ => false end) (map (edges g v) (nodup(vertex g)))).
-    
 
-Definition bipartite (g : graph) := exists (f : nat -> bool), forall ( u v : nat), In u (vertex g) /\ In v (vertex g) /\ (edges g u v = Ptrue) -> not (f u = f v).
-
+Definition bipartite (g : graph) := exists (f : nat -> bool), forall ( u v : nat), (edges g u v = Ptrue) -> not (f u = f v).
 
 Definition edges1 (x y : nat) : BoolPlus :=
   match x, y with
@@ -123,8 +122,6 @@ Proof.
   unfold bipartite.
   exists (fun x => match x with | 1 | 3 => true | _ => false end).
   intros.
-  destruct H.
-  destruct H0.
   destruct u.
   destruct v.
   discriminate.
@@ -168,12 +165,274 @@ Proof.
   destruct v.
   discriminate.
   destruct v.
-  lia.
+  discriminate.
   destruct v.
   discriminate.
   destruct v.
   lia.
+  discriminate.
+Qed. 
+
+
+Definition correctcol (g : graph) (f : nat -> nat) := forall (u v : nat), (edges g u v = Ptrue) -> f u <> f v.
+
+Definition coloring1 (x : nat) : nat :=
+  match x with
+  | 1 => 1
+  | 2 => 2
+  | 3 => 1
+  | _ => 0
+  end.
+
+Theorem example_coloring_is_correct : correctcol ex1 coloring1.
+Proof.
+  unfold correctcol.
+  intros.
+  destruct v.
+  destruct u.
+  simpl.
+  discriminate.
+  destruct u.
+  unfold not.
+  discriminate.
+  unfold not.
+  destruct u.
+  discriminate.
+  destruct u.
+  discriminate.
+  simpl.
+  discriminate.
+  destruct u.
+  destruct v.
+  unfold not.
+  discriminate.
+  unfold not.
+  discriminate.
+  destruct u.
+  destruct v.
+  unfold not.
+  discriminate.
+  destruct v.
+  simpl.
+  trivial.
+  destruct v.
+  simpl.
+  discriminate.
+  simpl.
+  lia.
+  destruct u.
+  destruct v.
+  unfold not.
+  discriminate.
+  destruct v.
+  simpl.
+  discriminate.
+  destruct v.
+  simpl.
+  lia.
+  simpl.
+  lia. 
+  destruct u.
+  destruct v.
+  unfold not.
+  discriminate.
+  destruct v.
+  simpl.
+  trivial.
+  destruct v.
+  simpl.
+  discriminate.
+  simpl.
+  lia. 
+  destruct v.
+  unfold not.
+  discriminate.
+  destruct v.
+  simpl.
+  trivial.
+  destruct v.
+  simpl.
+  discriminate.
+  simpl.
   discriminate.
 Qed.
 
+Theorem bipartite_is_bicoloring : forall (g : graph), bipartite g -> exists (col : nat -> nat), correctcol g col.
 
+Proof.
+  unfold bipartite in *.
+  intros.
+  destruct H.
+  exists (fun v => match x v with | true => 1 | false => 2 end).
+  unfold correctcol.
+  intros.
+  specialize (H u v).
+  apply H in H0.
+  unfold not in*.
+  destruct (x u).
+  destruct (x v).
+  exfalso.
+  apply H0.
+  trivial.
+  discriminate.
+  destruct (x v).
+  discriminate.
+  exfalso.
+  apply H0.
+  trivial.
+Qed.
+
+Definition edges2 (x y : nat) : BoolPlus :=
+  match x, y with
+  | 1, 2 | 2, 3 | 3, 4 | 4, 1 | 2, 1 | 3, 2 | 4, 3 | 1, 4 => Ptrue
+  | 1, 3 | 3, 1 | 2, 4 | 4, 2 => Pfalse
+  | _, _ => Pbzdura
+  end.
+  
+Theorem dowod2: forall (x : nat), In x [ 1; 2; 3; 4 ] \/ (forall y, edges2 x y = Pbzdura /\ edges2 y x = Pbzdura).
+Proof.
+  intros.
+  destruct x.
+  right.
+  destruct y.
+  split.
+  trivial.
+  trivial.
+  split.
+  trivial.
+  destruct y.
+  trivial.
+  destruct y.
+  trivial.
+  destruct y.
+  trivial.
+  destruct y.
+  trivial.
+  simpl.
+  trivial.
+  destruct x.
+  left.
+  unfold In.
+  left.
+  trivial.
+  destruct x.
+  left.
+  unfold In.
+  right.
+  left.
+  trivial.
+  destruct x.
+  left.
+  unfold In.
+  right.
+  right.
+  left.
+  trivial.
+  destruct x.
+  left.
+  unfold In.
+  right.
+  right.
+  right.
+  left.
+  trivial.
+  right.
+  destruct y.
+  simpl.
+  split.
+  trivial.
+  trivial.
+  split.
+  simpl.
+  trivial.
+  unfold edges2.
+  destruct y.
+  trivial.
+  destruct y.
+  trivial.
+  destruct y.
+  trivial.
+  destruct y.
+  trivial.
+  trivial.
+Qed.
+  
+Definition C_4 := graph1 [ 1; 2; 3; 4 ] edges2 dowod2.
+
+Eval compute in C_4.
+
+Theorem C_4_is_bipartite : bipartite C_4.
+Proof.
+  unfold bipartite.
+  exists (fun x => match x with | 1 | 3 => true | _ => false end).
+  intros.
+  destruct u.
+  destruct v.
+  discriminate.
+  destruct v.
+  lia.
+  destruct v.
+  discriminate.
+  destruct v.
+  lia.
+  discriminate.
+  destruct u.
+  destruct v.
+  lia.
+  destruct v.
+  discriminate.
+  destruct v.
+  lia.
+  destruct v.
+  discriminate.
+  lia.
+  destruct u.
+  destruct v.
+  discriminate.
+  destruct v.
+  lia.
+  destruct v.
+  discriminate.
+  destruct v.
+  lia.
+  destruct v.
+  discriminate.
+  destruct v.
+  discriminate.
+  discriminate.
+  destruct u.
+  destruct v.
+  lia.
+  destruct v.
+  discriminate.
+  destruct v.
+  lia.
+  destruct v.
+  discriminate.
+  lia.
+  destruct v.
+  destruct u.
+  discriminate.
+  discriminate.
+  destruct v.
+  discriminate.
+  destruct v.
+  destruct u.
+  discriminate.
+  discriminate.
+  destruct v.
+  lia.
+  destruct v.
+  destruct u.
+  discriminate.
+  discriminate.
+  destruct u.
+  discriminate.
+  discriminate.
+Qed. 
+
+Theorem C_4_is_bicol : exists (col : nat -> nat), correctcol C_4 col.
+Proof.
+  apply (bipartite_is_bicoloring C_4).
+  apply C_4_is_bipartite.
+Qed.
